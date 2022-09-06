ce91f50e1200d94f4d29b76e4a26698171e57c30
Jason Ernst <ernstjason1@gmail.com>
2022-09-06 13:29:44 -0700

    fix instrumentation test coverage

--- app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.kt
@@ -1,8 +1,11 @@
 package com.example.myapplication
 
+import androidx.test.core.app.ActivityScenario
 import androidx.test.platform.app.InstrumentationRegistry
+import de.mannodermaus.junit5.ActivityScenarioExtension
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.RegisterExtension
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -10,10 +13,17 @@ import org.junit.jupiter.api.Test
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleInstrumentedTest {
+    @RegisterExtension
+    var scenarioExtension = ActivityScenarioExtension.launch(
+        MainActivity::class.java
+    )
+
     @Test
-    fun useAppContext() {
+    fun useAppContext(scenario: ActivityScenario<MainActivity>) {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.myapplication", appContext.packageName)
+
+        scenario.onActivity { }
     }
 }
