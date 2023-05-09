4a04726c4f662e06f30f250e4e8861060bf53ddf
Jason Ernst <ernstjason1@gmail.com>
2023-05-09 10:17:20 -0700

    Test filtering (#116)
    * WiP, trying to get test filtering to work
    
    * added test filtering

--- app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.kt
@@ -3,9 +3,11 @@ package com.example.myapplication
 import androidx.test.platform.app.InstrumentationRegistry
 import de.mannodermaus.junit5.ActivityScenarioExtension
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Timeout
 import org.junit.jupiter.api.extension.RegisterExtension
+import org.slf4j.LoggerFactory
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,6 +16,8 @@ import org.junit.jupiter.api.extension.RegisterExtension
  */
 @Timeout(30)
 class ExampleInstrumentedTest {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     @JvmField
     @RegisterExtension
     var scenarioExtension = ActivityScenarioExtension.launch(
@@ -21,6 +25,7 @@ class ExampleInstrumentedTest {
     )
 
     @Test
+    @Tag("OTHERTEST")
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
@@ -28,5 +33,11 @@ class ExampleInstrumentedTest {
 
         val scenario = scenarioExtension.scenario
         scenario.onActivity { }
+    }
+
+    @Test
+    @Tag("TAGTEST")
+    fun test2() {
+        logger.debug("test2")
     }
 }
