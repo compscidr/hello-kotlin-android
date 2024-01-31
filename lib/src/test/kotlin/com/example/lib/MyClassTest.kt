83b6eb7a7b41fddf1f88b64d4d4bb476f60a0bbc
Jason Ernst <ernstjason1@gmail.com>
2024-01-30 17:16:19 -0800

    Added lib tests, included jvm tests for android

---
+++ lib/src/test/kotlin/com/example/lib/MyClassTest.kt
@@ -0,0 +1,11 @@
+package com.example.lib
+
+import org.junit.jupiter.api.Test
+
+class MyClassTest {
+    @Test
+    fun testMyClass() {
+        val myClass = MyClass()
+        myClass.display()
+    }
+}
